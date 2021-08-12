6c2604467777ffe326f2a4d56cbef82097cc6ab9
amanshitta-crossml <amanshitta@crossml.com>
2021-08-12 12:19:05 +0530

    Models Corrected ManyToMany Relationship

--- learn/Music/models.py
+++ learn/Music/models.py
@@ -7,10 +7,22 @@ class Singer(models.Model):
 	Model for Singer for a music
 	"""
 	name  = models.CharField(max_length=200)
-	age = models.IntegerField()
 
 	def __str__(self):
 		return self.name
+
+
+class Album(models.Model):
+	"""
+	Model for albums with one to many relation with songs
+	"""
+	# song = models.ForeignKey(Song, on_delete=models.CASCADE)
+	album_name = models.CharField(max_length=200)
+	release_date = models.DateField('release date')
+
+	def __str__(self):
+		return self.album_name
+
 
 class Song(models.Model):
 	"""
@@ -18,24 +30,14 @@ class Song(models.Model):
 	Many to many relation with singer
 	"""
 
+	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 	song_name = models.CharField(max_length=300)
-	song_genre = models.CharField(max_length=200)
-	# song_length = models.DurationField()
+	# song_genre = models.CharField(max_length=200)
+	song_length = models.DecimalField(max_digits=4, decimal_places=2)
 	singer = models.ManyToManyField(Singer)
 
 	def __str__(self):
 		return self.song_name
 
-
-class Album(models.Model):
-	"""
-	Model for albums with one to many relation with songs
-	"""
-	song = models.ForeignKey(Song, on_delete=models.CASCADE)
-	album_name = models.CharField(max_length=200)
-	release_date = models.DateTimeField('release date')
-
-	def __str__(self):
-		return self.album_name
 
 
