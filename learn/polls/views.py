468276cd09e35633c5a62947d282c97d8e4e652c
amanshitta-crossml <amanshitta@crossml.com>
2021-08-17 13:21:27 +0530

    Polls work - strated

--- learn/polls/views.py
+++ learn/polls/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question, Choice
 
-# Create your views here.
 
 
 def index(request):
-	return HttpResponse("Working View.")
+	question_list = Question.objects.all()
+	context = {'question_list':question_list}
+	return render(request, 'polls/index.html', context)
+    # return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at %s" % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	print(question)
+	# breakpoint()
+	context = {'question':question}
+
+	return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
